b30acee7cfea76c1bb99cfdcad72e491c399bd19
Matías Vega <elmativega3@gmail.com>
2022-10-31 00:11:20 -0300

    Deploy Railway

--- api/src/db.js
+++ api/src/db.js
@@ -5,12 +5,12 @@ const path = require('path');
 
 
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, BD_DEPLOY
+  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
 } = process.env;
 
-const sequelize = new Sequelize(BD_DEPLOY, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false, 
+  native: false, 
 });
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pi-pokemon`, {
