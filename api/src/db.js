19a61c0e7a4d7087e93406eca3155ebc40d62f52
Matías Vega <elmativega3@gmail.com>
2022-10-30 22:45:54 -0300

    deploy Railway

--- api/src/db.js
+++ api/src/db.js
@@ -2,23 +2,24 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-// const {
-//   PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE
-// } = process.env;
 
-// const sequelize = new Sequelize(`postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}`, {
+
+const {
+  DB_USER, DB_PASSWORD, DB_HOST, BD_DEPLOY
+} = process.env;
+
+const sequelize = new Sequelize(BD_DEPLOY, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
+
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pi-pokemon`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
 
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pi-pokemon`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
